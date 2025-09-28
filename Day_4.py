fb45a2cc8debbc819814335f43b22178080d0fba
Faizan Ali <faizan.ali.datascience@gmail.com>
2025-09-28 21:21:45 +0500

    Add files via upload

---
+++ Day_4.py
@@ -0,0 +1,84 @@
+# first_value = 10
+# first_value2 = 20
+
+# print(first_value)
+
+# print("Hello World")
+
+# Separator first value
+
+# first_value = "Python"
+# second_value = "Programming"
+
+# print(first_value, second_value)
+# Separator second value
+
+# first_value2 = "Python2"
+# second_value2 = "Programming2"
+
+# separator = first_value2, second_value2
+# print(type
+
+# first_value = 10
+# second_value = 10.6
+# third_value = "PYthon prog"
+# forth_value = 'A'
+
+# we have to give user input 
+# print( type (first_value))
+
+# frist = 10
+# second = 10.566
+# third = "Hhello world"
+# print(int(frist))
+# print(float(second))
+# print(type(second))
+# print(type(third))
+
+# result_compare = frist_value > second_value
+# print("Our condition is this:", frist <= second)
+
+#print("Our condition is this:", frist <= second)
+
+
+
+
+
+
+#typecasting ...
+# num_str = "10" # value ko koi bi 
+# num_int = int(num_str) # change variable kiye
+# print("your value is this", type(num_int), num_str) 
+
+# num = 25 # value ko assign koi value? 
+# num_str = str(num)
+# print("your value is this",type(num_str), num_str) 
+
+# # Convert float to integer
+# pi = 3.14
+# pi_int = int(pi)
+# print("your value is this", type(pi_int), pi_int) 
+
+
+
+#user input 
+
+
+# name = input("Enter your name: ") # function this correct way
+# print(type(name))
+
+# name = input("Enter your name: ") # function this correct way
+# print(name)
+
+first_value = int(input("Please enter first value: "))
+second_value = int(input("Please second first value: "))
+
+# Arithmetic operators? 
+
+result = first_value + second_value
+print("Your sum is this:", result)
+
+
+
+
+

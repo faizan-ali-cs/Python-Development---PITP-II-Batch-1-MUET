12a175c86a429d4a5fa9120ababc2620af4e7b0c
Faizan Ali <faizan.ali.datascience@gmail.com>
2025-09-28 21:11:49 +0500

    Add files via upload

---
+++ learning_varables_and_datatypes.py
@@ -0,0 +1,39 @@
+
+Math = 50
+CS = 40
+Urdu = 70
+English = 60
+Pk = 80
+Total = 500
+
+Obtain_marks = Math+CS+Urdu+English+Pk
+print("Our obtain is: " , Obtain_marks/500*100)
+print("Percentage is:")
+
+
+first_value = 10
+second_value = 20.5
+thired_value = "Python"
+foruth_value = 'A'
+
+print(first_value, second_value, thired_value)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

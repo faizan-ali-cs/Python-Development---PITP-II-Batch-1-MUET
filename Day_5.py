2cf732dff60269a94727642ed7d3481451ae4c4e
Faizan Ali <faizan.ali.datascience@gmail.com>
2025-09-28 21:22:25 +0500

    Add files via upload

---
+++ Day_5.py
@@ -0,0 +1,94 @@
+# x = 2
+# y *= 2
+
+# # 2 x 2 = 4
+
+# print("Our condition is this: ", y)
+
+
+# x = 5
+# x %= 4
+
+# print(x) # Assignement operator 
+
+# x = 6 # even number
+# y = 7 # odd number
+
+# print(x % y) # Modulus
+
+
+# identity operator? 
+
+# x = 10
+# y = 10
+
+# print(x ! y)
+
+# x = 10
+# y = 10
+
+# print(x != y) # assignement operator 
+
+# x = 10
+# y = 9
+
+# print(x is y)
+
+
+# x = 10
+# x = 9
+
+# print(x)
+
+
+# x = 10
+# x = 10
+
+# print(x is not x)
+
+
+# AND operator
+
+x = 10
+y = 10
+
+print(x & y)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# value_one= int(input("Enter a first number:"))
+# value_one= int(input("Enter a second number:"))
+
+# sum = value_one + value_one
+
+# print("The sum is:", sum)
+
+
+
+
+
+
+
+
+
+
+
+# x = 10
+# x % 9 # Modulas
+# print(x)
